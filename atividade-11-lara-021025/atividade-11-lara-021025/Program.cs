f3532e227cfdc0751bf1a48109ca58479608ec0f
laraangeli0812-lab <laraangeli0812@gmail.com>
2025-10-02 17:17:06 -0300

    Add files via upload

---
+++ atividade-11-lara-021025/atividade-11-lara-021025/Program.cs
@@ -0,0 +1,20 @@
+﻿//declaração de variáveis.
+
+using System.ComponentModel.Design;
+
+Console.WriteLine("digite o ano atual: ");
+int anoatual = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("digite seu ano de nascimento: ");
+int anonascimento = int.Parse(Console.ReadLine() ?? "0");
+
+int idade = anoatual - anonascimento;
+
+Console.WriteLine($"idade: {idade} anos.");
+
+if (idade > 16)
+    Console.WriteLine("você não pode votar esse ano.");
+else if (idade == 16 || idade == 17)
+    Console.WriteLine("voto facultativo.");
+else if (idade >= 18 && idade <= 69)
+    Console.WriteLine("voto obrigatório.");
