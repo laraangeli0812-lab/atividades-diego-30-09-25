76029d231716a8674888e1334a3aa0594f55ed83
laraangeli0812-lab <laraangeli0812@gmail.com>
2025-10-02 15:53:48 -0300

    Add files via upload

---
+++ atividade-2-lara-021025/atividade-2-lara-021025/Program.cs
@@ -0,0 +1,9 @@
+﻿//declaração de variáveis
+
+int nu1, nu2;
+
+Console.WriteLine("Digite um número:");
+nu1 = int.Parse(Console.ReadLine());
+nu2 = (nu1 - 1);
+
+Console.WriteLine($"o antecessor de {nu1} é {nu2}.");
