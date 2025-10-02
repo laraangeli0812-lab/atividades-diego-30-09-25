a9d73ca4fad7d3d2adfa2360ec58a0ba7fb50831
laraangeli0812-lab <laraangeli0812@gmail.com>
2025-10-02 15:54:26 -0300

    Add files via upload

---
+++ atividade-3-lara-021025/atividade-3-lara-021025/Program.cs
@@ -0,0 +1,10 @@
+﻿//declaração de variáveis
+
+Console.WriteLine("Digite a base do retângulo: ");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite a altura do retângulo: ");
+double a = double.Parse(Console.ReadLine());
+
+double area = b * a;
+
+Console.WriteLine($"a área do retângulo é {area}");
