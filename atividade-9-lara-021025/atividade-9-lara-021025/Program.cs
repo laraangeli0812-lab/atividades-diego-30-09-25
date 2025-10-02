90611a584f357a9d431a7c1d2921fbef5a866333
laraangeli0812-lab <laraangeli0812@gmail.com>
2025-10-02 16:40:02 -0300

    Add files via upload

---
+++ atividade-9-lara-021025/atividade-9-lara-021025/Program.cs
@@ -0,0 +1,13 @@
+﻿//declaração de variáveis
+
+Console.WriteLine("digite a quantidade de maças compradas: ");
+
+int quantidade = int.Parse(Console.ReadLine());
+
+decimal precouni = (quantidade < 12) ? 1.30m : 1.00m;
+decimal total = precouni * quantidade;
+
+Console.WriteLine();
+Console.WriteLine($"quantidade : {quantidade}");
+Console.WriteLine($"preço unitário: {precouni.ToString("c")}");
+Console.WriteLine($"custo total da compra de maçãs: {total.ToString("c")}");
