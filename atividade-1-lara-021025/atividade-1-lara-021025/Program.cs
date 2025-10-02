6559bffc7c36f79173df68cf1e5f6b68beff151b
laraangeli0812-lab <laraangeli0812@gmail.com>
2025-10-02 15:53:04 -0300

    Add files via upload

---
+++ atividade-1-lara-021025/atividade-1-lara-021025/Program.cs
@@ -0,0 +1,18 @@
+﻿//declaração de variáveis
+
+int a = 10, b = 20, c = 0;
+
+/*
+console.writeline("Digite um valor pra A:");
+a = int.parse(console.readline());
+console.writeline("Digite um valor pra B:");
+b = int.parse(console.readline());
+*/
+
+Console.WriteLine($"O valor ANTERIOR de: A = {a}, B = {b}");
+
+c = a;
+a = b;
+b = c;
+
+Console.WriteLine($"O valor ATUAL de: A = {a}, B = {b}");
