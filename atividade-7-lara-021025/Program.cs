c4824eb8e3652385a1f0bf6a4435188596d96d81
laraangeli0812-lab <laraangeli0812@gmail.com>
2025-10-02 15:55:07 -0300

    Add files via upload

---
+++ atividade-7-lara-021025/Program.cs
@@ -0,0 +1,14 @@
+﻿//declaração de variáveis
+
+int nu1;
+
+Console.WriteLine("Digite um número.");
+nu1 = int.Parse(Console.ReadLine());
+
+if (nu1 > 10) {
+    Console.WriteLine($"O número {nu1} é MAIOR que 10.");
+}
+else
+{
+    Console.WriteLine($"O número {nu1} é MENOR que 10.");
+}
