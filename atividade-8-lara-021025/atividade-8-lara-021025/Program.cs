400f40efee8d7747da38845a9a3606e35d015339
laraangeli0812-lab <laraangeli0812@gmail.com>
2025-10-02 16:10:59 -0300

    Add files via upload

---
+++ atividade-8-lara-021025/atividade-8-lara-021025/Program.cs
@@ -0,0 +1,40 @@
+﻿//declaração de variáveis
+
+namespace VerificarValorPositivoOuNegativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            while (true)
+            {
+                Console.Write("Digite um número (diferente de zero): ");
+
+                bool entradaValida = int.TryParse(Console.ReadLine(), out numero);
+
+                if (!entradaValida)
+                {
+                    Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro.");
+                }
+                else if (numero == 0)
+                {
+                    Console.WriteLine("Valor zero não é permitido. Tente novamente.");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (numero > 0)
+            {
+                Console.WriteLine("O número é POSITIVO.");
+            }
+            else
+            {
+                Console.WriteLine("O número é NEGATIVO.");
+            }
+        }
+    }
+}
