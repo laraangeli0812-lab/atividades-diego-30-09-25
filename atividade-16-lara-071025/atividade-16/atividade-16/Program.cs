0ba35f8f480a8b8f83bca74ad60c086f50e9f3ad
laraangeli0812-lab <laraangeli0812@gmail.com>
2025-10-07 14:06:39 -0300

    Add files via upload

---
+++ atividade-16-lara-071025/atividade-16/atividade-16/Program.cs
@@ -0,0 +1,31 @@
+﻿//declaração de variáveis
+
+namespace FatorialApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            long fatorial = 1;
+
+            Console.WriteLine("=== Cálculo de Fatorial ===");
+            Console.Write("Digite um número inteiro: ");
+            n = int.Parse(Console.ReadLine());
+
+            if (n < 0)
+            {
+                Console.WriteLine("Fatorial de número negativo não existe!");
+            }
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    fatorial *= i;
+                }
+
+                Console.WriteLine($"O fatorial de {n} é: {fatorial}");
+            }
+        }
+    }
+}
