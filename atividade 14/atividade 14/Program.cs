36afd0ac0e722b65abd4664594df9fcbf12374bd
laraangeli0812-lab <laraangeli0812@gmail.com>
2025-10-07 14:33:32 -0300

    Add files via upload

---
+++ atividade 14/atividade 14/Program.cs
@@ -0,0 +1,9 @@
+﻿// declaração de variáveis
+
+Console.WriteLine("digite um valor N (maior que 0): ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"\nNumeros de 1 ate {N}:");
+for (int i = 1; i <= N; i++)
+               
+Console.WriteLine(i);
