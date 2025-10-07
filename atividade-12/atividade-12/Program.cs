e4e4bf77a3809a292073e3f5357c1a5f22fa67f2
laraangeli0812-lab <laraangeli0812@gmail.com>
2025-10-07 14:15:02 -0300

    Add files via upload

---
+++ atividade-12/atividade-12/Program.cs
@@ -0,0 +1,11 @@
+﻿//declaração de variáveis
+
+Console.WriteLine("digite o primeiro valor: ");
+double a = double.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("digite o segundo valor: ");
+double b = double.Parse(Console.ReadLine() ?? "0");
+
+double maior = (a > b) ? a : b;
+
+Console.WriteLine($"o maior valor é: {maior}");
