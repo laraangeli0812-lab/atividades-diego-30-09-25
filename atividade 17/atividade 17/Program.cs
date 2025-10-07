b932297365a93ab5326e0369ba37fbf7cc3f3e9a
laraangeli0812-lab <laraangeli0812@gmail.com>
2025-10-07 15:08:57 -0300

    Add files via upload

---
+++ atividade 17/atividade 17/Program.cs
@@ -0,0 +1,25 @@
+﻿//declaração de variáveis
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a quantidade de valores a serem lidos: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int negativos = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Digite o {i + 1}º valor: ");
+            double valor = double.Parse(Console.ReadLine());
+
+            if (valor < 0)
+            {
+                negativos++;
+            }
+        }
+
+        Console.WriteLine($"\nQuantidade de valores negativos: {negativos}");
+    }
+}
