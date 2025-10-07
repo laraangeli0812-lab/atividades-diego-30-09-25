1367f922a614eea896c7962722ffefcc59949369
laraangeli0812-lab <laraangeli0812@gmail.com>
2025-10-07 14:24:29 -0300

    Add files via upload

---
+++ atividade 13/atividade 13/Program.cs
@@ -0,0 +1,16 @@
+﻿//declaração de variáveis
+
+Console.Write("digite o primeiro valor: ");
+double a = double.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("digite o segundo valor: ");
+double b = double.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("digite o terceiro valor: ");
+double c = double.Parse(Console.ReadLine() ?? "0");
+
+double[] arr = new double[] { a, b, c };
+Array.Sort(arr);
+
+Console.WriteLine("valores em ordem crescente: ");
+Console.WriteLine($"{arr[0]}, {arr[1]}, {arr[2]}");
