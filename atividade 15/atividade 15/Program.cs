43026a25e995becd76d2e62c8f02b9cf6d74fd75
laraangeli0812-lab <laraangeli0812@gmail.com>
2025-10-07 14:47:02 -0300

    Add files via upload

---
+++ atividade 15/atividade 15/Program.cs
@@ -0,0 +1,6 @@
+﻿//declaração de variáveis
+
+Console.WriteLine("numeros entre 1000 e 2000 que deixam resto 5 na divisão por 11:");
+for (int i = 1000; i <= 2000; i++)
+    if (i % 11 == 5)
+        Console.WriteLine(i);
